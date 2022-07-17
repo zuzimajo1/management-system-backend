330eb1a5725ee85c891734e58b15327c282f5a73
Zuzim <ajozuzim>
2022-07-18 00:09:29 +0800

    changes

--- controllers/EventController.js
+++ controllers/EventController.js
@@ -1,6 +1,8 @@
 const db = require("../models")
 const Events = db.events;
 const { Op } = require("sequelize");
+const dayjs = require("dayjs");
+const date = new Date();
 
 
 //create Event
@@ -25,4 +27,49 @@ const EventGetAll = async (req, res)=>{
     }
 }
 
-module.exports = { EventCreation, EventGetAll };
+//Delete Single Event
+
+const DeleteEvent = async (req, res)=>{
+    try {
+        const deleteevent = await Events.destroy({
+            where:{
+                id: req.params.id,
+            },
+        });
+        res.status(200).json(deleteevent)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+}
+
+//Find Single Event
+
+const FindSingleEvent = async (req, res)=>{
+    try{
+        const findevent = await Events.findOne({
+          where: {
+            //start and end must be both correct to get the data
+            [Op.and]: [
+              {
+                //the start must be least than or equal to today's date
+                start: {
+                  [Op.lte]: dayjs(date).format("YYYY-MM-DD"),
+                },
+              },
+              {
+                //the end must be greater than or equal to today's date
+                end: {
+                  [Op.gte]: dayjs(date).format("YYYY-MM-DD"),
+                },
+              },
+            ],
+          },
+        });
+        res.status(200).json(findevent)
+    }catch(err){
+        res.status(500).json(err)
+    }
+}
+
+
+module.exports = { EventCreation, EventGetAll, DeleteEvent, FindSingleEvent };
--- routers/EventRouter.js
+++ routers/EventRouter.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const { EventCreation, EventGetAll } = require("../controllers/EventController");
+const {
+  EventCreation,
+  EventGetAll,
+  DeleteEvent,
+  FindSingleEvent,
+} = require("../controllers/EventController");
 
 
 //Routers
-router.route("/").post(EventCreation).get(EventGetAll);
+router.route("/").post(EventCreation).get(EventGetAll)
+router.route("/eventtoday").get(FindSingleEvent);
+router.route("/:id").delete(DeleteEvent);
 
 module.exports = router;
